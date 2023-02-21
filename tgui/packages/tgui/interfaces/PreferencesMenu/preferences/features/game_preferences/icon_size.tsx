5fc33b1f221361a9af9c0e98f52e2fb107f8f86d
RimiNosha <riminosha@gmail.com>
2023-02-21 19:00:22 +0000

    Fuck

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/icon_size.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/icon_size.tsx
@@ -1,8 +1,9 @@
-import { Feature, FeatureNumberInput } from "../base";
+import { Feature, FeatureNumberInput } from '../base';
 
 export const icon_size: Feature<number> = {
-  name: "Icon Size",
-  category: "UI",
-  description: "The icon size of your viewport. Larger values are suitable for larger screens. Too small of numbers may not render correctly",
+  name: 'Icon Size',
+  category: 'UI',
+  description:
+    'The icon size of your viewport. Larger values are suitable for larger screens. Too small of numbers may not render correctly',
   component: FeatureNumberInput,
 };
