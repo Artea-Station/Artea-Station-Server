0f5c736c8ad355e7322d18dacb87e401e14ccd5c
RimiNosha <riminosha@gmail.com>
2023-03-10 01:12:00 +0000

    Fucking dumb cunt twat I hate hate hate hate everything

--- tools/ci/check_grep.sh
+++ tools/ci/check_grep.sh
@@ -256,14 +256,14 @@ if grep -P 'addtimer\((?=.*TIMER_OVERRIDE)(?!.*TIMER_UNIQUE).*\)' code/**/*.dm; 
 fi;
 
 unit_test_files="code/modules/unit_tests/**/**.dm"
-if $grep 'allocate\(/mob/living/carbon/human[,\)]' $unit_test_files ||
-	$grep 'new /mob/living/carbon/human\s?\(' $unit_test_files ||
-	$grep 'var/mob/living/carbon/human/\w+\s?=\s?new' $unit_test_files ; then
+if grep 'allocate\(/mob/living/carbon/human[,\)]' $unit_test_files ||
+	grep 'new /mob/living/carbon/human\s?\(' $unit_test_files ||
+	grep 'var/mob/living/carbon/human/\w+\s?=\s?new' $unit_test_files ; then
 	echo
 	echo -e "${RED}ERROR: Usage of mob/living/carbon/human detected in a unit test, please use mob/living/carbon/human/consistent.${NC}"
 	st=1
 fi;
-if $grep '^/*var/' code/**/*.dm; then
+if grep '^/*var/' code/**/*.dm; then
 	echo
     echo -e "${RED}ERROR: Unmanaged global var use detected in code, please use the helpers.${NC}"
     st=1
