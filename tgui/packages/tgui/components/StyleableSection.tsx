a03ed7809f4004cc9e1ba9093bedc6143de8337a
Rimi Nosha <riminosha@gmail.com>
2023-10-26 05:01:21 +0100

    Quirks Menu Refit! (#341)

--- tgui/packages/tgui/components/StyleableSection.tsx
+++ tgui/packages/tgui/components/StyleableSection.tsx
@@ -9,9 +9,12 @@ export const StyleableSection: SFC<{
   childStyle?;
   title?;
   titleSubtext?;
+  scrollable?;
 }> = (props) => {
   return (
-    <Box style={props.style}>
+    <Box
+      style={props.style}
+      className={props.scrollable && 'Section--scrollable'}>
       {/* Yes, this box (line above) is missing the "Section" class. This is very intentional, as the layout looks *ugly* with it.*/}
       <Box class="Section__title" style={props.titleStyle}>
         <Box class="Section__titleText" style={props.textStyle}>
