b9bc840d828265706d3c1b8e58679b1474c1327f
Rimi Nosha <riminosha@gmail.com>
2024-12-23 01:31:57 +0000

    External Lib Fix-Ups (#622)
    ## About The Pull Request
    
    Who knew that being inactive for nearly 6 months would cause things to
    slowly break?
    
    https://github.com/tgstation/tgstation/pull/80955
    https://github.com/tgstation/tgstation/pull/83635
    https://github.com/tgstation/tgstation/pull/88181

--- dependencies.sh
+++ dependencies.sh
@@ -8,20 +8,20 @@ export BYOND_MAJOR=515
 export BYOND_MINOR=1639
 
 #rust_g git tag
-export RUST_G_VERSION=3.0.0
+export RUST_G_VERSION=3.5.1
 
 #node version
 export NODE_VERSION=16
 export NODE_VERSION_PRECISE=18.14.2
 
 # SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.8
+export SPACEMAN_DMM_VERSION=suite-1.9
 
 # Python version for mapmerge and other tools
 export PYTHON_VERSION=3.9.0
 
 #hypnagogic repo
-export CUTTER_REPO=actioninja/hypnagogic
+export CUTTER_REPO=spacestation13/hypnagogic
 
 #hypnagogic git tag
-export CUTTER_VERSION=v3.0.1
+export CUTTER_VERSION=v4.0.0
